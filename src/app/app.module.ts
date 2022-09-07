95cad1d9237dddbc56444a7abac01882fc8de5fc
Rian <riana@titusgt.com>
2022-09-07 09:57:18 +0800

    login and dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+import {SidebarModule} from 'primeng/sidebar';
+import {Table, TableModule} from 'primeng/table';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputTextModule,
+    FormsModule,
+    ButtonModule,
+    CardModule,
+    SidebarModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import employeeData from '../../assets/employees.json';
+@Component({
+  
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+
+})
+export class DashboardComponent implements OnInit {
+
+  employees = employeeData;
+  
+  display = false;
+
+  ngOnInit()
+  {
+  }
+
+}
